5ecdc2d476a85814525a3c457c15304536f1edc4
ahmedfathy997 <Ahmed.Fathy@itworx.com>
2024-02-11 19:41:08 +0200

    Add Sloution

---
+++ WiredBrainCoffe.shopInfoTool/WiredBrainCoffe.shopInfoTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WiredBrainCoffe.shopInfoTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
