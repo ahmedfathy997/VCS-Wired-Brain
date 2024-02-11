d98bec98d1bc69c2999dd9568ee8716e0ad6e984
ahmedfathy997 <Ahmed.Fathy@itworx.com>
2024-02-11 19:42:30 +0200

    Change Program

--- WiredBrainCoffe.shopInfoTool/WiredBrainCoffe.shopInfoTool/Program.cs
+++ WiredBrainCoffe.shopInfoTool/WiredBrainCoffe.shopInfoTool/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Coffe shop Wired brain");
         }
     }
 }
